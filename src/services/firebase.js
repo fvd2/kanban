3cd85e97089719aa0894aecc177f81f012bc36cf
fvd2 <fmvd1931@outlook.com>
2021-09-22 10:36:39 +0200

    installed recaptcha app check

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,18 +1,22 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
+import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check'
 
 export const app = initializeApp({
-	apiKey: 'AIzaSyDwkoDdGaD7u9en9tnXKizNQYlTdEHLaBE',
-	authDomain: 'kanban-218d1.firebaseapp.com',
-	databaseURL:
-		'https://kanban-218d1-default-rtdb.europe-west1.firebasedatabase.app',
-	projectId: 'kanban-218d1',
-	storageBucket: 'kanban-218d1.appspot.com',
-	messagingSenderId: '759366593327',
-	appId: '1:759366593327:web:0da81e7b41d2097ca70fda',
-	measurementId: 'G-XZSFXHBR8L'
+	apiKey: process.env.FIREBASE_APIKEY,
+	authDomain: process.env.FIREBASE_AUTHDOMAIN,
+	databaseURL: process.env.FIREBASE_AUTHDOMAIN,
+	projectId: process.env.FIREBASE_PROJECTID,
+	storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+	messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+	appId: process.env.FIREBASE_APPID,
+	measurementId: process.env.FIREBASE_MEASUREMENTID
 })
+
+const appCheck = initializeAppCheck(app, {
+	provider: new ReCaptchaV3Provider(process.env.RECAPTCHA_SITE_KEY),  
+	isTokenAutoRefreshEnabled: true})
 
 export const db = getFirestore(app)
 export const auth = getAuth(app)
